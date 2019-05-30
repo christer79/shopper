004699bdfbd5eff6e56d4e9615907cc495a5fc8f
Christer Eriksson <christer.j.eriksson@gmail.com>
2019-05-30 17:34:50 +0200

    Adding CORS to backend plus setting default port=3500

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/rs/cors"
+
 	"github.com/christer79/shopper/backend/internal/app/shopper"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -17,16 +19,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const defaultPort = "8080"
-
-const (
-	host         = "localhost"
-	port         = 5432
-	user         = "postgres"
-	password     = "docker"
-	dbname       = "shopping"
-	initializeDB = false
-)
+const defaultPort = "3500"
 
 func main() {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
@@ -44,9 +37,19 @@ func main() {
 		port = defaultPort
 	}
 
+	c := cors.New(cors.Options{
+		AllowedOrigins:     []string{"*"}, // "http://localhost:3000", "http://localhost:3500"
+		AllowedMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions, http.MethodPatch},
+		AllowedHeaders:     []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "Connection"},
+		ExposedHeaders:     []string{"Link", "Connection", "Upgrade", "Sec-WebSocket-Key", "Sec-WebSocket-Version"},
+		AllowCredentials:   true,
+		Debug:              true,
+		OptionsPassthrough: true,
+	})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/playground", handler.Playground("GraphQL playground", "/graphql")).Methods("GET")
-	r.HandleFunc("/graphql", handler.GraphQL(app.NewExecutableSchema(app.Config{Resolvers: &app.Resolver{DB: db}}))).Methods("GET", "POST")
+	r.Handle("/graphql", c.Handler(handler.GraphQL(app.NewExecutableSchema(app.Config{Resolvers: &app.Resolver{DB: db}})))).Methods("GET", "POST", "OPTIONS")
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("build/static"))))
 	r.PathPrefix("/web").Handler(http.StripPrefix("/", http.FileServer(http.Dir("build"))))
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
--- backend/go.mod
+++ backend/go.mod
@@ -5,5 +5,6 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gorilla/mux v1.7.2
 	github.com/lib/pq v1.1.1
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser v1.1.2
 )
