1dabc605ed1bb4f898dae097b7c82361b8faad9c
Christer Eriksson <christer.j.eriksson@gmail.com>
2019-08-13 20:55:10 +0200

    List of lists is updated when creating new lists

--- frontend/src/components/selectlist/listcreator/index.js
+++ frontend/src/components/selectlist/listcreator/index.js
@@ -2,9 +2,18 @@ import React from "react";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 
+const LISTS = gql`
+  {
+    lists {
+      id
+      name
+    }
+  }
+`;
+
 const CREATE_LIST = gql`
   mutation CreateList($id: ID!, $name: String!, $listtype: String!) {
-    createList(input: { id: $id, name: $name, listtype: $listtype}) {
+    createList(input: { id: $id, name: $name, listtype: $listtype }) {
       name
       id
     }
@@ -13,11 +22,12 @@ const CREATE_LIST = gql`
 
 function ListCreator() {
   const [form, setValues] = React.useState({
-    name: "", listtype: "shopping"
+    name: "",
+    listtype: "shopping"
   });
 
   const updateField = e => {
-    console.log("Updating field: ", e.target.name, " with: ", e.target.value)
+    console.log("Updating field: ", e.target.name, " with: ", e.target.value);
     setValues({
       ...form,
       [e.target.name]: e.target.value
@@ -40,7 +50,12 @@ function ListCreator() {
                       .substr(2, 9),
                   name: form.name,
                   listtype: form.listtype
-                }
+                },
+                refetchQueries: [
+                  {
+                    query: LISTS
+                  }
+                ]
               });
               setValues({ name: "" });
             }}
