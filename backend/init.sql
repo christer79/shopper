d5687f47dc840b493412ab43e4a1339c39e20135
Christer Eriksson <christer.j.eriksson@gmail.com>
2019-08-16 19:48:18 +0200

    Setting db version in init.sql

--- backend/init.sql
+++ backend/init.sql
@@ -1,2 +1,18 @@
 DROP TABLE lists;
-CREATE TABLE lists(id SERIAL PRIMARY KEY, list_id VARCHAR(50) NOT NULL, list_name VARCHAR(50) NOT NULL, list_type VARCHAR(50) NOT NULL, user_uid VARCHAR(50), owner BOOLEAN);
+CREATE TABLE lists
+(
+    id SERIAL PRIMARY KEY,
+    list_id VARCHAR(50) NOT NULL,
+    list_name VARCHAR(50) NOT NULL,
+    list_type VARCHAR(50) NOT NULL,
+    user_uid VARCHAR(50),
+    owner BOOLEAN
+);
+CREATE TABLE version
+(
+    id SERIAL PRIMARY KEY,
+    nr INTEGER NOT NULL
+)
+INSERT INTO version
+    (nr)
+VALUES(3)
