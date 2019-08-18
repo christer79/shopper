e07413791f3fb8931082b9817401f4e66d225a24
Christer Eriksson <christer.j.eriksson@gmail.com>
2019-08-18 10:12:43 +0200

    Removed a missplaced Typography and changed some attributes to silence errors. Added a key to a list.

--- frontend/src/components/selectlist/listslist/index.js
+++ frontend/src/components/selectlist/listslist/index.js
@@ -15,11 +15,11 @@ const mapDispatchToProps = {
 };
 
 function Error(props) {
-  return <div>Error loading: {props.error}</div>;
+  return <Typography>Error loading: + {props.error}</Typography>;
 }
 
 function Loading() {
-  return <div>Loading</div>;
+  return <Typography>Loading</Typography>;
 }
 
 function ListsList() {
@@ -28,31 +28,29 @@ function ListsList() {
       <Typography variant="h5" component="h3">
         Select list
       </Typography>
-      <Typography component="p">
-        <Grid container spacing={4} style={{ padding: 24 }}>
-          <Query query={LISTS} errorPolicy="all">
-            {({ error, loading, data, ...result }) => {
-              if (error) return <Error error={error} />;
-              if (loading) return <Loading />;
-              console.log(data.lists);
-              return data.lists ? (
-                data.lists.map(list => (
-                  <Grid item xs={12} sm={6} lg={4} xl={3}>
-                    <List
-                      key={list.id}
-                      name={list.name}
-                      type={list.listtype}
-                      id={list.id}
-                    />
-                  </Grid>
-                ))
-              ) : (
-                <div>No lists yet!</div>
-              );
-            }}
-          </Query>
-        </Grid>
-      </Typography>
+      <Grid container spacing={4} style={{ padding: 24 }}>
+        <Query query={LISTS} errorPolicy="all">
+          {({ error, loading, data, ...result }) => {
+            if (error) return <Error error={error} />;
+            if (loading) return <Loading />;
+            console.log(data.lists);
+            return data.lists ? (
+              data.lists.map(list => (
+                <Grid key={list.id} item xs={12} sm={6} lg={4} xl={3}>
+                  <List
+                    key={list.id}
+                    name={list.name}
+                    type={list.listtype}
+                    id={list.id}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <span>No lists yet!</span>
+            );
+          }}
+        </Query>
+      </Grid>
     </Paper>
   );
 }
