612fe33525a901a12d2ab1acfd9f36b15f2ca21c
Christer Eriksson <christer.j.eriksson@gmail.com>
2020-01-04 11:58:04 +0100

    Converted cleanDB to podman

--- postgres/cleanDB.sh
+++ postgres/cleanDB.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash 
 
-docker rm -f -v  $(docker ps -aqf "name=pg-docker")
+podman rm -f -v  $(podman ps -aqf "name=pg-docker")
 
 sudo rm -rf  /home/chreri/docker/volumes/postgres
-
-docker build -t custom_postgres .
-docker run --rm --name pg-docker -e POSTGRES_DB=shopping -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v /home/chreri/docker/volumes/postgres:/var/lib/postgresql/data custom_postgres
+mkdir -p /home/chreri/docker/volumes/postgres
+podman build -t custom_postgres .
+podman run --rm --name pg-docker -e POSTGRES_DB=shopping -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v /home/chreri/docker/volumes/postgres:/var/lib/postgresql/data custom_postgres
 
